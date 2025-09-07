d13791e48818739de75aabf52dd824dd139e52c4
youngberry1 <abdulabdul28@aol.com>
2025-09-07 17:01:06 +0000

    updated worker.js

--- worker.js
+++ worker.js
@@ -1,9 +1,7 @@
 onmessage = (e) => {
   if (e.data === 'start') {
     for (i = 1; i <= 100000000; i++) {
-      if (i % 10000000 == 0) {
-        postMessage(`Progress ${i}`);
-      }
+      postMessage(`Progress ${i}`);
     }
     postMessage('done');
   }
